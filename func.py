69a6325b44287cad00e166ada2a2a92ce01bb8f3
KowsalyaS18 <87513120+KowsalyaS18@users.noreply.github.com>
2021-07-18 16:24:34 +0530

    Create func.py
    2.	Define a class which has at least two methods:
    getString: to get a string from console input
    printString: to print the string in upper case.

---
+++ func.py
@@ -0,0 +1,14 @@
+class myclass:
+    def __init__(self):
+        self.a = ""
+
+    def getString(self):
+        self.a = input()
+
+    def printString(self):
+        print(self.s.upper())
+
+strObj = myclass()
+strObj.getString()
+strObj.printString()
+
