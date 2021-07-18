14c87c1076965231bc001859e642a334aea8dd41
KowsalyaS18 <87513120+KowsalyaS18@users.noreply.github.com>
2021-07-18 16:41:27 +0530

    Update listsum.py

--- listsum.py
+++ listsum.py
@@ -1,7 +1,13 @@
+def my_func(list1):
+    print(sum(list1))
+
+
+
 list1=[]
 n=int(input("enter the  list size"))
 for i in range(0,n):
     item=int(input())
     list1.append(item)
 print (list1)
-print(sum(list1))
+
+my_func(list1)
